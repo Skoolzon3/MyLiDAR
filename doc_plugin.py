b21e628d0645e8028ed3c696bc030e03fb1518ef
Skoolzon3 <pablofergon03@gmail.com>
2025-07-17 10:13:13 +0200

    Extend selection of entire categories to all currently present

--- doc_plugin.py
+++ doc_plugin.py
@@ -1,11 +1,8 @@
 import os
 from qgis.PyQt.QtCore import QCoreApplication
-from qgis.PyQt.QtWidgets import QAction, QFileDialog, QMessageBox, QDialogButtonBox
+from qgis.PyQt.QtWidgets import QAction, QFileDialog, QMessageBox, QDialogButtonBox, QDialog
 from qgis.PyQt.QtGui import QIcon
-
-# User interface imports
 from qgis.PyQt import uic
-from qgis.PyQt.QtWidgets import QDialog
 
 # Backend imports
 import laspy
@@ -35,6 +32,9 @@ class ReportDialog(QDialog, form_class):
         self.setupUi(self)
 
         self.groupTime.toggled.connect(self.on_group_time_toggled)
+        self.groupIntensity.toggled.connect(self.on_group_intensity_toggled)
+        self.groupSpatial.toggled.connect(self.on_group_spatial_toggled)
+        self.groupFileMetadata.toggled.connect(self.on_group_file_metadata_toggled)
 
         self.checkboxes = [
             # Metadata checkboxes
@@ -89,6 +89,75 @@ class ReportDialog(QDialog, form_class):
         else:
             self.checkMinTime.setChecked(False)
             self.checkMaxTime.setChecked(False)
+
+        self.update_ok_button()
+
+    def on_group_intensity_toggled(self, checked):
+        self.checkMinIntensity.setEnabled(checked)
+        self.checkMaxIntensity.setEnabled(checked)
+
+        if checked:
+            self.checkMinIntensity.setChecked(True)
+            self.checkMaxIntensity.setChecked(True)
+        else:
+            self.checkMinIntensity.setChecked(False)
+            self.checkMaxIntensity.setChecked(False)
+
+        self.update_ok_button()
+
+    def on_group_spatial_toggled(self, checked):
+        self.checkNumPoints.setEnabled(checked)
+        self.checkArea.setEnabled(checked)
+        self.checkDensity.setEnabled(checked)
+        self.checkBounds.setEnabled(checked)
+        self.checkXAxisBounds.setEnabled(checked)
+        self.checkYAxisBounds.setEnabled(checked)
+
+        if checked:
+            self.checkNumPoints.setChecked(True)
+            self.checkArea.setChecked(True)
+            self.checkDensity.setChecked(True)
+            self.checkBounds.setChecked(True)
+            self.checkXAxisBounds.setChecked(True)
+            self.checkYAxisBounds.setChecked(True)
+        else:
+            self.checkNumPoints.setChecked(False)
+            self.checkArea.setChecked(False)
+            self.checkDensity.setChecked(False)
+            self.checkBounds.setChecked(False)
+            self.checkXAxisBounds.setChecked(False)
+            self.checkYAxisBounds.setChecked(False)
+
+        self.update_ok_button()
+
+    def on_group_file_metadata_toggled(self, checked):
+        self.checkFileName.setEnabled(checked)
+        self.checkFileSource.setEnabled(checked)
+        self.checkGlobalEncoding.setEnabled(checked)
+        self.checkSystemId.setEnabled(checked)
+        self.checkGenSoftware.setEnabled(checked)
+        self.checkVersion.setEnabled(checked)
+        self.checkPointFormat.setEnabled(checked)
+        self.checkCreationDate.setEnabled(checked)
+
+        if checked:
+            self.checkFileName.setChecked(True)
+            self.checkFileSource.setChecked(True)
+            self.checkGlobalEncoding.setChecked(True)
+            self.checkSystemId.setChecked(True)
+            self.checkGenSoftware.setChecked(True)
+            self.checkVersion.setChecked(True)
+            self.checkPointFormat.setChecked(True)
+            self.checkCreationDate.setChecked(True)
+        else:
+            self.checkFileName.setChecked(False)
+            self.checkFileSource.setChecked(False)
+            self.checkGlobalEncoding.setChecked(False)
+            self.checkSystemId.setChecked(False)
+            self.checkGenSoftware.setChecked(False)
+            self.checkVersion.setChecked(False)
+            self.checkPointFormat.setChecked(False)
+            self.checkCreationDate.setChecked(False)
 
         self.update_ok_button()
 
